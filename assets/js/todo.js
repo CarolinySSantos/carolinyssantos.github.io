068e59225238b22dee59b2cf931fbd0bc9ac3b5b
Carol Santana dos Santos <c.santana.dos.santos@avanade.com>
2022-05-03 15:16:47 -0300

    Aula7

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,3 +1,5 @@
+import { addTask, getAllTasks } from "./utils/domFunctions.js";
+
 const db = [
 
 {
@@ -17,23 +19,19 @@ const db = [
 {
 id: 2,
     title: "Aula 4 Fiap - Avanade",
-steps: [
-    {step: "Ajustar textos" },
-    {step: "Trocar imagens para imagens públicas "},
-    {step: "Publicar no Expo"},
-    {step: "Publicar no Expo Store Connect"},
-],
-done: false,
-dueDate: "2022-05-06",
-reminder: "2022-05-06 10:00",
-},
-
+steps: [{step: "Atributos Globais" }, {step: "Estrutura CSS"}],
+    done: true,
+    dueDate: "2022-04-28",
+    },
 ];
 
-console.log(db[0].title);
+getAllTasks(db);
 
-const newTask = document.querySelector("#inputTxtNewTask");
+//console.log(db[0].title);
+
 const form = document.querySelector("#addNewTask");
+const newTask = document.querySelector("#inputTxtNewTask");
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 });
@@ -41,10 +39,21 @@ newTask.addEventListener("keyup",(e) => {
     e.preventDefault();
     e.stopPropagation();
     if(e.key == "Enter") {
-       alert(newTask.value);
+        if (!newTask.value) {
+       alert("Digite uma nova tarefa, para adicionar.");
+        } else {
+        alert(newTask.value);
 
-     db.push({ id: Number(db.length) + 1, title: newTask.value });
-     newTask.value = "";
+        const d = new Date();
+        const today = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+        
+        db.push({ id: Number(db.length) + 1, title: newTask.value, done:false, dueDate: today, });
+     
+        document.querySelector(".tasks").innerHTML = "";
+        getAllTasks(db);
+
+        newTask.value = "";
+        console.log(db);
+        }
     }
-    console.log(db);
 });
